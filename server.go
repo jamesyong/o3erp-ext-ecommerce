43d3fa9ff75f75f968a99cfd38601b2b482de174
jamesyong <james.yong.erp@gmail.com>
2014-11-06 21:51:21 +0800

    refactoring

--- server.go
+++ server.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	frontend "github.com/jamesyong/o3erp/frontend"
+	"github.com/jamesyong/o3erp/frontend/config"
+	"os"
 )
 
+func init() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	config.WORKING_DIRECTORY = pwd
+	config.PATH_BASE_FRONTEND_TEMPLATES = pwd + "/../o3erp/frontend/templates"
+	config.PATH_BASE_FRONTEND_ASSETS = pwd + "/../o3erp/frontend/assets"
+}
+
 func main() {
-
 	frontend.Startup()
-
 }
